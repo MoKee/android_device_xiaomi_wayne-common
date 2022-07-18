960f5198fc3167ba6b1f43844b460ca53017446a
Martincz Gao <martincz@mokeedev.com>
2022-07-18 12:17:05 +0800

    wayne-common: mokee-ify

--- wayne.mk
+++ wayne.mk
@@ -13,7 +13,7 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-mokee
 
 # Audio
 PRODUCT_COPY_FILES += \
